0a5d12fd44abcb75d233f2d8217abb7b8508c384
MayankGupta0119 <mayankgupta0119@gmail.com>
2025-07-05 22:54:47 +0530

    table preview

--- app/(main)/account/_components/AccountChart.jsx
+++ app/(main)/account/_components/AccountChart.jsx
@@ -1,7 +1,168 @@
-import React from "react";
+"use client";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { endOfDay, format, startOfDay, subDays } from "date-fns";
+import React, { useMemo, useState } from "react";
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Legend,
+  Rectangle,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 
-const AccountChart  = () => {
-  return <div></div>;
+const DATE_RANGES = {
+  "7D": { label: "Last 7 days", days: 7 },
+  "1M": { label: "Last 1 Month", days: 30 },
+  "3M": { label: "Last 3 Months", days: 90 },
+  "6M": { label: "Last 6 Months", days: 180 },
+  "1Y": { label: "Last 1 Year", days: 365 },
+  ALL: { label: "All time", days: null },
+};
+const AccountChart = ({ transactions }) => {
+  const [dateRange, setDateRange] = useState("1M");
+  const filteredData = useMemo(() => {
+    const range = DATE_RANGES[dateRange];
+    const now = new Date();
+    const startDate = range.days
+      ? startOfDay(subDays(now, range.days))
+      : startOfDay(new Date(0));
+    //filter all transactions within the date range
+    const filtered = transactions.filter(
+      (t) => new Date(t.date) >= startDate && new Date(t.date) <= endOfDay(now)
+    );
+
+    //group by date and sum the amounts
+    const grouped = filtered.reduce((acc, transaction) => {
+      const date = format(new Date(transaction.date), "MMM dd");
+      if (!acc[date]) {
+        acc[date] = { date, income: 0, expense: 0 };
+      }
+      if (transaction.type === "INCOME") {
+        acc[date].income += transaction.amount;
+      } else {
+        acc[date].expense += transaction.amount;
+      }
+      return acc;
+    }, {});
+
+    //convert to array and sort by date
+    return Object.values(grouped).sort((a, b) => {
+      return new Date(a.date) - new Date(b.date);
+    });
+  }, [transactions, dateRange]);
+  // console.log("filtered Data ==>", filteredData);
+  //calculate total income and expense for that particular date range
+  const total = useMemo(() => {
+    return filteredData.reduce(
+      (acc, day) => ({
+        income: acc.income + day.income,
+        expense: acc.expense + day.expense,
+      }),
+      { income: 0, expense: 0 }
+    );
+  }, [filteredData]);
+  return (
+    <Card>
+      <CardHeader
+        className={"flex flex-row items-center justify-between space-y-0 pb-7"}
+      >
+        <CardTitle className={"text-base font-normal"}>
+          Transaction Overview
+        </CardTitle>
+        <Select defaultValue={dateRange} onValueChange={setDateRange}>
+          <SelectTrigger className="w-[150px]">
+            <SelectValue placeholder="Select Range" />
+          </SelectTrigger>
+          <SelectContent>
+            {Object.entries(DATE_RANGES).map(([key, { label }]) => (
+              <SelectItem key={key} value={key}>
+                {label}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+      </CardHeader>
+      <CardContent>
+        <div className="flex justify-around mb-6 text-sm">
+          <div className="text-center">
+            <p className="text-muted-foreground">Total Income</p>
+            <p className="text-lg font-bold text-green-500">
+              RS {total.income.toFixed(2)}
+            </p>
+          </div>
+          <div className="text-center">
+            <p className="text-muted-foreground">Total Epenses</p>
+            <p className="text-lg font-bold text-red-500">
+              RS {total.expense.toFixed(2)}
+            </p>
+          </div>
+          <div className="text-center">
+            <p className="text-muted-foreground">Net</p>
+            <p
+              className={`text-lg font-bold ${
+                total.income - total.expense >= 0
+                  ? "text-green-500"
+                  : "text-red-500"
+              }`}
+            >
+              RS {(total.income - total.expense).toFixed(2)}
+            </p>
+          </div>
+        </div>
+        <div className="h-[300px]">
+          <BarChart
+            data={filteredData}
+            margin={{
+              top: 10,
+              right: 10,
+              left: 10,
+              bottom: 0,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" vertical={"false"} />
+            <XAxis dataKey="date" />
+            <YAxis
+              fontSize={12}
+              tickLine={false}
+              axisLine={false}
+              tickFormatter={(value) => `Rs${value}`}
+            />
+            <Tooltip />
+            <Legend />
+            <Bar
+              dataKey="income"
+              name={"Income"}
+              fill="#8884d8"
+              radius={[4,4,0,0]}
+            />
+            <Bar
+              dataKey="expense"
+              name={"Expense"}    
+              fill="#82ca9d"
+              radius={[4,4,0,0]}
+            />
+          </BarChart>
+        </div>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default AccountChart;
