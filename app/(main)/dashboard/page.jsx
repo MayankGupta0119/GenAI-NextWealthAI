cd27b1853b9b02b6df0c9f0251a949e07d9510de
MayankGupta0119 <mayankgupta0119@gmail.com>
2025-07-06 12:21:08 +0530

    add Budget

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -4,14 +4,26 @@ import { Card, CardContent } from "@/components/ui/card";
 import { Plus } from "lucide-react";
 import React from "react";
 import AccountCard from "./_components/AccountCard";
+import { getCurrentBudget } from "@/actions/budget";
+import BudgetProgress from "./_components/BudgetProgress";
 
 const DashboardPage = async () => {
   const accounts = await getUserAccounts();
   // console.log(accounts);
+  const defaultAccount = accounts.find((account) => account.isDefault);
+  let budgetData = null;
+  if (defaultAccount) {
+    budgetData = await getCurrentBudget(defaultAccount.id);
+  }
   return (
-    <div className="px-5">
-      {/*Budget Progree  */}
-
+    <div className="space-y-8">
+      {/*Budget Progress  */}
+      {defaultAccount && (
+        <BudgetProgress
+          initialBudget={budgetData?.budget || 0}
+          currentExpenses={budgetData?.currentExpenses || 0}
+        />
+      )}
       {/* Overview */}
 
       {/* Accounts Grid */}
